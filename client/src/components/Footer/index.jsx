8f3d83072f1f478ceb11ce1ffff9298ac8acb934
Matthew <matthew_carpenter@icloud.com>
2019-12-27 17:18:27 -0700

    fixed issue with build. pushing empty footer to the internet.

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css';
+
+function Footer() {
+    return (
+        <div className="footer_root">
+            <div className="footer_one"></div>
+            <div className="footer_two"></div>
+            <div className="footer_three"></div>
+            <div className="footer_four"></div>
+        </div>
+    )
+}
+
+export default Footer;
