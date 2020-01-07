2b567534b1e1a5bb1da79a8bb641445b2f4ec832
Christopher Ducey <chrisducey01@me.com>
2020-01-06 19:19:24 -0700

    Added lead summary to LeadWrapper component

--- client/src/components/LeadWrapper/index.jsx
+++ client/src/components/LeadWrapper/index.jsx
@@ -17,22 +17,22 @@ class LeadWrapper extends Component {
         this.getLeads();
     }
 
-    updateLead(id, obj){
-        axios.put(`/api/customers/id/${id}`,{custObj: obj})
-        .then(res=>{
-            console.log(res);
-            this.getLeads();
-        })
-        .catch(err=>{
-            console.log(err);
-        })
+    updateLead(id, obj) {
+        axios.put(`/api/customers/id/${id}`, { custObj: obj })
+            .then(res => {
+                console.log(res);
+                this.getLeads();
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     getLeads() {
         axios.get('/api/customers/leads')
             .then(res => {
                 console.log(res.data);
-                this.setState({leadsArr: res.data})
+                this.setState({ leadsArr: res.data })
             })
             .catch(err => {
                 console.log(err);
@@ -41,30 +41,42 @@ class LeadWrapper extends Component {
 
     render() {
         return (
-            <div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Signup Date</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                        this.state.leadsArr.map(lead=>{
-                            const signupDate = moment(lead.createdAt).format('MM/DD/YYYY');
-                            return (
-                                <Lead id={lead._id} onClick={this.updateLead}
-                                    firstName={lead.firstName} lastName={lead.lastName}
-                                    signupDate={signupDate}
-                                />
-                            )
-                        })}
-                        
-                    </tbody>
-                </table>
+            <div className="container">
+                <div className="row py-3">
+                    <div className="col-4 text-center">
+                            <h3>Leads in Queue</h3>
+                            <h1>{this.state.leadsArr.length}</h1>
+                            <p>Not Yet Contacted</p>
+                    </div>
+                    <div className="col-8">
+
+                    </div>
+                </div>
+                <div className="row">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Signup Date</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.leadsArr.map(lead => {
+                                    const signupDate = moment(lead.createdAt).format('MM/DD/YYYY');
+                                    return (
+                                        <Lead id={lead._id} onClick={this.updateLead}
+                                            firstName={lead.firstName} lastName={lead.lastName}
+                                            signupDate={signupDate}
+                                        />
+                                    )
+                                })}
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
