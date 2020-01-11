463fd8b34a2ab461dcefab063fa6ac3d4d1ad4fd
Matthew <matthew_carpenter@icloud.com>
2020-01-11 12:26:56 -0700

    updated gallery component to access photos in the root level using our customer root level api call.

--- client/src/components/Gallery/index.jsx
+++ client/src/components/Gallery/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './gallery.css';
 import bathroom from './images/bathroom.JPG';
 import kitchen1 from './images/kitchen1.JPG';
@@ -15,47 +15,37 @@ import greenKitchen from './images/green-kitchen.JPG'
 
 
 
-export default class Gallery extends Component {
-    render() {
+export default function Gallery() {
+
         return (
+            
             <div className="gallery_root">
 
                 <div className="gallery_container">
 
-                    <div className="item item-1" style={{backgroundImage: `url(${kitchen1})`}}>
-                        <span></span>
-                    </div>
-                    <div className="item item-2" style={{backgroundImage: `url(${bathroom})`}}></div>
+                    <div className="item item_1"></div>
+                    <div className="item item_2"></div>
+                    <div className="item span-3 item_3"></div>
+                    <div className="item item_4"></div>
+                    <div className="item span-2 item_5"></div>
 
-                    <div className="item span-3 item-3" style={{backgroundImage: `url(${kitchen3})`}}></div>
+                    <div className="item span-2 item_6"></div>
+                    <div className="item item_7"></div>
+                    <div className="item span-3 item_8"></div>
+                    <div className="item item_9"></div>
+                    <div className="item  item_10"></div>
+                    <div className="item item_11"></div>
 
-                    <div className="item item-4" style={{backgroundImage: `url(${kitchen4})`}}></div>
-
-                    <div className="item span-2" style={{backgroundImage: `url(${kitchen5})`}}></div>
-
-                    <div className="item span-2 item-6" style={{backgroundImage: `url(${woodenBar})`}}></div>
-                    
-                    <div className="item item-7" style={{backgroundImage: `url(${foldingBed})`}}></div>
-
-                    <div className="item span-3 item-8" style={{backgroundImage: `url(${Bed})`}}></div>
-
-                    <div className="item item-9" style={{backgroundImage: `url(${woodenCabinets})`}}></div>
-
-                    <div className="item  item-10" style={{backgroundImage: `url(${whiteCabinets})`}}></div>
-
-                    <div className="item item-11" style={{backgroundImage: `url(${bathroomCabinets})`}}></div>
-
+                    {/*     
+                    removed to preserve load times            
                     <div className="item span-2 item-12" style={{backgroundImage: `url(${greenKitchen})`}}></div>
+                    <div className="item span-2 item-12" style={{backgroundImage: `url(${greenKitchen})`}}></div> 
+                    */}
 
-                    <div className="item span-2 item-12" style={{backgroundImage: `url(${greenKitchen})`}}></div>
-
-                    <div className="item item-11" style={{backgroundImage: `url(${bathroomCabinets})`}}></div>
-
-
+                    <div className="item item_12"></div>
 
                 </div>
 
             </div>
         )
-    }
 }
