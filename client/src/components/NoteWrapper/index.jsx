6e71b70aa13075967c715d71dcc4c099dd4bf874
Christopher Ducey <chrisducey01@me.com>
2020-01-10 22:59:07 -0700

    Updated NoteWrapper to post notes to db

--- client/src/components/NoteWrapper/index.jsx
+++ client/src/components/NoteWrapper/index.jsx
@@ -10,23 +10,48 @@ class NoteWrapper extends Component {
         super(props)
         this.state = {
             toastShow: false,
+            username: null,
+            userid: null,
             id: props.id,
             notes: [],
             firstName: "",
             lastName: "",
             nickname: "",
             email: "",
-            phoneNumber: ""
+            phoneNumber: "",
+            newNoteText: ""
         }
         this.toggleToast = this.toggleToast.bind(this);
         this.addNote = this.addNote.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     componentDidMount() {
         if (this.state.id) {
             this.getNotesForContact();
         }
+        this.getUsername();
     }
+
+    // Getting username of logged in user to add to note entry
+    getUsername(){
+        axios.get('/auth/user')
+        .then(res=>{
+            console.log(res.data);
+            if(res.data) this.setState({
+                username: res.data.user.local.username,
+                userid: res.data.user._id
+            })
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+
+    handleInputChange(event) {
+        event.preventDefault();
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
     getNotesForContact() {
         axios.get(`/api/customers/id/${this.state.id}`)
@@ -52,7 +77,17 @@ class NoteWrapper extends Component {
 
     addNote(event) {
         event.preventDefault();
-        this.toggleToast();
+        console.log("Submit Note")
+        axios.post(`/api/customers/id/${this.state.id}/note`,{
+            content: this.state.newNoteText,
+            createdBy: this.state.userid,
+        })
+        .then(res=>{
+            window.location.href=`/crm/notes/${this.state.id}`
+        })
+        .catch(err=>{
+            console.log(err)
+        })
     }
 
     render() {
@@ -60,7 +95,7 @@ class NoteWrapper extends Component {
             <div className="m-3">
                 <ContactSearch hrefOnClick="/crm/notes" />
                 <button type="button" className="btn btn-dark mt-3"
-                    onClick={this.addNote}
+                    onClick={this.toggleToast} hidden={!this.state.id}
                 >Add a Note</button>
                 <hr />
                 <h1>Notes</h1>
@@ -70,7 +105,13 @@ class NoteWrapper extends Component {
                 <Toast show={this.state.toastShow} onClose={this.toggleToast}>
                     <form>
                         <h1>Add a Note</h1>
-                        <input type="textbox"/>
+                        <textarea className="form-control" id="newNoteText"
+                            name="newNoteText" value={this.state.newNoteText}
+                            onChange={this.handleInputChange}
+                        />
+                        <button type="submit" className="btn btn-primary my-3"
+                            onClick={this.addNote}
+                        >Submit</button>
                     </form>
 
                 </Toast>
