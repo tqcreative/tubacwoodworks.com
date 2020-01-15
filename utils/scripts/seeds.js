1613e57fd3877fc9d984c927b4bbfacb4c5ae3c8
Diana Gerardo <dianagerardo@email.arizona.edu>
2020-01-14 22:40:52 -0700

    Adding routes to state-image component

--- utils/scripts/seeds.js
+++ utils/scripts/seeds.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const db = require("../../models");
+let seed_imgs = require ("./seed_img")
+let seed_portfolio = require ('./seed_portfolio');
+let seed_partners = require ('./seed_partner');
 
 // This file empties the collections
 
@@ -8,94 +11,33 @@ mongoose.connect(
   "mongodb://localhost/projectthree"
 );
 
-//////////
-// 6 Portfolio links
-//////////
-const portfolioSeed = [
-  {
-      "title" : "Kitchen & Bath",
-      "link" : "localhost3000/kitchenbathvanity",
-      "imgUrl" : "https://lda.lowes.com/is/image/Lowes/DP18-143027_1-1_dt_KitchenCountertops_2col_hero?scl=1",
-      "date" : new Date(Date.now())
-  },
-  {
-      "title" : "Furniture",
-      "link" : "localhost3000/furniture",
-      "imgUrl" : "https://lda.lowes.com/is/image/Lowes/DP18-143027_1-1_dt_KitchenCountertops_2col_hero?scl=1",
-      "date" : new Date(Date.now())
-  },
-  {
-      "title" : "Commercial",
-      "link" : "localhost3000/commercial",
-      "imgUrl" : "https://lda.lowes.com/is/image/Lowes/DP18-143027_1-1_dt_KitchenCountertops_2col_hero?scl=1",
-      "date" : new Date(Date.now())
-  },
-  {
-      "title" : "Pro Tips",
-      "link" : "localhost3000/protips",
-      "imgUrl" : "https://lda.lowes.com/is/image/Lowes/DP18-143027_1-1_dt_KitchenCountertops_2col_hero?scl=1",
-      "date" : new Date(Date.now())
-  },
-  {
-      "title" : "Gallery",
-      "link" : "localhost3000/gallery",
-      "imgUrl" : "https://lda.lowes.com/is/image/Lowes/DP18-143027_1-1_dt_KitchenCountertops_2col_hero?scl=1",
-      "date" : new Date(Date.now())
-  },
-  {
-      "title" : "Get A Quote!",
-      "link" : "localhost3000/signup",
-      "imgUrl" : "https://lda.lowes.com/is/image/Lowes/DP18-143027_1-1_dt_KitchenCountertops_2col_hero?scl=1",
-      "date" : new Date(Date.now())
-  }
-];
-
-//////////
-// 4 Partners
-//////////
-const partnerSeed = [
-  {
-    "title" : "Woodworks",
-    "link" : "www.google.com",
-    "imgUrl" : "static/media/partnerOne.jpg",
-    "date" : new Date(Date.now())
-  },
-  {
-    "title" : "Tile",
-    "link" : "www.google.com",
-    "imgUrl" : "static/media/partnerTwo.jpg",
-    "date" : new Date(Date.now())
-  },
-  {
-    "title" : "Dry Wall",
-    "link" : "www.google.com",
-    "imgUrl" : "static/media/partnerThree.jpg",
-    "date" : new Date(Date.now())
-  },
-  {
-    "title" : "Doors & Windows",
-    "link" : "www.google.com",
-    "imgUrl" : "static/media/partnerFour.jpg",
-    "date" : new Date(Date.now())
-  }
-]
-
 ////////////
 // delete and seed database.
 ////////////
 
 db.Portfolio
 .remove({})
-.then(() => db.Portfolio.collection.insertMany(portfolioSeed))
+.then(() => db.Portfolio.collection.insertMany(seed_portfolio))
 .then(data => {
   console.log(data.result.n + " portfolio content seeded.");
 
-  db.Partners.remove({})
-  .then(() => db.Partners.collection.insertMany(partnerSeed))
+  db.Partners
+  .remove({})
+  .then(() => db.Partners.collection.insertMany(seed_partners))
   .then(data => {
     console.log(data.result.n + " partners seeded.");
-    process.exit(0);
+
+    db.Images
+    .remove({})
+    .then(() => db.Images.collection.insertMany(seed_imgs))
+    .then(data => {
+      console.log(data.result.n + " images seeded.")
+      process.exit(0);
+    })
+    
   })
+
+
 
 
 })
