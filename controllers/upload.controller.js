ce229284dbe5aa49c04f22c3c354b5a03f103c93
Matthew <matthew_carpenter@icloud.com>
2020-01-18 14:05:35 -0700

    created post route that will push to the imageArray, NOT the kitchen, bath or whatever tables tho. cool stuff.

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -2,17 +2,45 @@ const db = require("../models");
 const router = require("express").Router();
 
 // Add (post) an appointment for 
-// router.use("/cms/uploadfile/hash43b4h234bhj", uploadController);
+// router.use("/cms/uploadfile/hash43b4h234bhj/:id", uploadController);
 // root level of the router
-router.route("/")
-    .get((req, res) => {
-        console.log(req.body);
-
+router.route("/:id")
+    .post((req, res) => {
+        //console.log(req.params.id);
+        let imageNameToBeAddedToTheArray = req.params.id;
+        console.log(imageNameToBeAddedToTheArray);
         db.Images
         .find()
         .then(dbReturns => {
+            // console.log(dbReturns[0]._id);
             console.log(dbReturns);
-            res.json(dbReturns)
+            let thisArrayId = dbReturns[0]._id;
+
+            db.Images.update(
+                { $push: { imageArray: [imageNameToBeAddedToTheArray   ]}}
+            )
+            .then(
+                lastCall => {
+                    console.log("worked?")
+                }
+            )
+            .catch( err => {
+                res.send('rly?')
+            })
+
+
+
+
+            //imageNameToBeAddedToTheArray; can be used to add to the database.
+
+            // console.log(dbReturns);
+            // db.Images.findOneAndUpdate(thisArrayId, { $push: { imageArray: imageNameToBeAddedToTheArray } })
+
+            // res.json(dbReturns)
+
+            //{ $push: { <field1>: <value1>, ... } }
+
+
         })
         .catch(err => {
                 res.send('do you even code bah?')
