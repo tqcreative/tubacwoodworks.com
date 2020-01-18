a8168aebe0f9c1d153be0861dd7f4d20d90d540e
Christopher Ducey <chrisducey01@me.com>
2020-01-18 10:27:15 -0700

    Updated employee appts component on employees page to have a header

--- client/src/components/EmployeeAppts/index.jsx
+++ client/src/components/EmployeeAppts/index.jsx
@@ -3,32 +3,39 @@ import moment from 'moment';
 
 function EmployeeAppts(props) {
     return (
-        <div className="row">
-            <div className="col m-2">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Time</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Details</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.appointments.map(appointment => {
-                            return (
-                                <tr key={appointment._id}>
-                                    <td>{moment(appointment.date).format('MM/DD/YYYY')}</td>
-                                    <td>{moment(appointment.date).format('h:mm a')}</td>
-                                    <td>{appointment.customer.firstName}</td>
-                                    <td>{appointment.customer.lastName}</td>
-                                    <td>{appointment.detail}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
+        <div>
+            <div className="row">
+                <div className="col m-2">
+                    <h3>Your Scheduled Appts</h3>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col m-2">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.appointments.map(appointment => {
+                                return (
+                                    <tr key={appointment._id}>
+                                        <td>{moment(appointment.date).format('MM/DD/YYYY')}</td>
+                                        <td>{moment(appointment.date).format('h:mm a')}</td>
+                                        <td>{appointment.customer.firstName}</td>
+                                        <td>{appointment.customer.lastName}</td>
+                                        <td>{appointment.detail}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
