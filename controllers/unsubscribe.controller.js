036a1133b7b2b21e16c8d28b65a8a4cd08ffa7c1
Matthew <matthew_carpenter@icloud.com>
2020-01-21 21:27:35 -0700

    we can unsub peeps

---
+++ controllers/unsubscribe.controller.js
@@ -0,0 +1,12 @@
+const db = require("../models");
+const router = require("express").Router();
+
+// Update 
+router.route("/:info").put((req, res) => {
+    db.Customer
+        .findOneAndUpdate({ email: req.params.info }, {$set: { promoContactByEmail: false }}, req.body)
+        .then(dbModel => res.send("You have unsubscribed. sorta... we still got yo' data."))
+        .catch(err => res.status(422).json(err));
+})
+
+module.exports =  router;
