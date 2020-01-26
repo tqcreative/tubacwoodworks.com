927f53db5c84c7b3304ce8bc14dabd95ffa11ee0
Matthew <matthew_carpenter@icloud.com>
2020-01-25 22:18:30 -0700

    running seed on database.

---
+++ client/src/sub_component/PencilEdit/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './pencilEdit.css';
+
+export default function index(props) {
+    return (
+        <div>
+            <ion-icon name="create"></ion-icon>
+        </div>
+    )
+}
