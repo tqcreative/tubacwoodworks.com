e3ab1196374ec1cd4504a266ed34b999eb1b9973
Matthew <matthew_carpenter@icloud.com>
2020-02-09 12:39:24 -0700

    updated seed file for new photo urls.

--- utils/scripts/seed_portfolio.js
+++ utils/scripts/seed_portfolio.js
@@ -2,37 +2,37 @@ let seed_portfolio = [
     {
         "title" : "Kitchen & Bath",
         "link" : "localhost3000/kitchenbathvanity",
-        "imgUrl": `/cms/images/portfolio_1.JPG`,
+        "imgUrl": `https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_1.JPG`,
         "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor." 
     },
     {
         "title" : "Furniture",
         "link" : "localhost3000/furniture",
-        "imgUrl": `/cms/images/portfolio_2.JPG`,
+        "imgUrl": `https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_2.JPG`,
         "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor." 
     },
     {
         "title" : "Commercial",
         "link" : "localhost3000/commercial",
-        "imgUrl": `/cms/images/portfolio_3.JPG`,
+        "imgUrl": `https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_3.JPG`,
         "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor." 
     },
     {
         "title" : "Pro Tips",
         "link" : "localhost3000/protips",
-        "imgUrl": `/cms/images/portfolio_4.JPG`,
+        "imgUrl": `https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_4.JPG`,
         "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor." 
     },
     {
         "title" : "Gallery",
         "link" : "localhost3000/gallery",
-        "imgUrl": `/cms/images/portfolio_5.JPG`,
+        "imgUrl": `https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_5.JPG`,
         "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor." 
     },
     {
         "title" : "Get A Quote!",
         "link" : "localhost3000/signup",
-        "imgUrl": `/cms/images/portfolio_6.JPG`,
+        "imgUrl": `https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_6.JPG`,
         "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor." 
     }
 ]
