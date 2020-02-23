871aec45f684b283c11737194ee4bcbf90c5014e
Matthew <matthew_carpenter@icloud.com>
2020-02-22 21:09:47 -0700

    updated partner urls to not click through when locked in as a user.

--- client/src/components/Partners/index.jsx
+++ client/src/components/Partners/index.jsx
@@ -10,6 +10,7 @@ let {partner_1, partner_2, partner_3, partner_text, login} = props.textContent;
 login == 'Peter' ?
 console.log('logged in') :
 console.log('not logged in');
+
     return (
         <div className="partners_root">
             <div className="partners_items_go_here">
@@ -20,7 +21,7 @@ console.log('not logged in');
                         <div id="partner_text" className="partner_card">
                             <h3>{partner_text.text}</h3>
                         </div>
-                        <div id="partner_card_1" className="partner_card" style={{ backgroundImage: `url(${partner_1.picture})` }} onClick={() => {window.open(partner_1.url)}}>
+                        <div id="partner_card_1" className="partner_card" style={{ backgroundImage: `url(${partner_1.picture})` }} onClick={() => {props.login === 'Peter' ? console.log('You must log out to click through to partner urls.') : window.open(partner_1.url) }}>
                             {props.login === 'Peter' ? <UploadPhoto __parent_image_name={"partner_1"} /> : <noscript></noscript>}
                             <div>
                                 <h3>{partner_1.name}</h3>
@@ -30,14 +31,14 @@ console.log('not logged in');
                     </div>
 
                     <div class="partner_pair">
-                        <div id="partner_card_2" className="partner_card" style={{ backgroundImage: `url(${partner_2.picture})` }} onClick={() => {window.open(partner_2.url)}}>
+                        <div id="partner_card_2" className="partner_card" style={{ backgroundImage: `url(${partner_2.picture})` }} onClick={() => {props.login === 'Peter' ? console.log('You must log out to click through to partner urls.') : window.open(partner_2.url) }}>
                             {props.login === 'Peter' ? <UploadPhoto __parent_image_name={"partner_2"} /> : <noscript></noscript>}
                             <div>
                             <h3>{partner_2.name}</h3>
                                 <p>{partner_2.description}</p>
                             </div>
                         </div>
-                        <div id="partner_card_3" className="partner_card" style={{ backgroundImage: `url(${partner_3.picture})` }} onClick={() => {window.open(partner_3.url)}}>
+                        <div id="partner_card_3" className="partner_card" style={{ backgroundImage: `url(${partner_3.picture})` }} onClick={() => {props.login === 'Peter' ? console.log('You must log out to click through to partner urls.') : window.open(partner_3.url) }}>
                             {props.login === 'Peter' ? <UploadPhoto __parent_image_name={"partner_3"} /> : <noscript></noscript>}
                             <div>
                             <h3>{partner_3.name}</h3>
