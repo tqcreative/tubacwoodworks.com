cb1663cf03cdbf2dba55ed7866fbd7dae5960275
Matthew <matthew_carpenter@icloud.com>
2020-05-30 15:45:37 -0700

    Started replacing gallery with a flex gallery.

---
+++ client/src/components/LightBox/LightBox.jsx
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import styled from 'styled-components';
+
+// LightBox.defaultProps = {
+//     children: <StyledPropChild>No Item</StyledPropChild>
+// }
+
+// export default function LightBox(props) {
+//     return (
+//         <React.Fragment>
+//             <StyledBackDrop/>
+//             <StyledLightBox>
+//                 {props.children}
+//             </StyledLightBox>
+//         </React.Fragment>
+//     )
+// }
+
+// const StyledLightBox = styled.div`
+//     z-index: 1;
+// `;
+
+// const StyledBackDrop = styled.div`
+//     position: fixed;
+//     left: 0;
+//     top: 0;
+//     background-color: rgba(0,0,0,0.5);
+//     z-index: 2;
+// `;
+
+// const StyledPropChild = styled.div`
+//     width: 80%;
+//     height: 80%;
+//     background-color: #fff;
+//     z-index: 3;
+//     color: #555;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+// `;
