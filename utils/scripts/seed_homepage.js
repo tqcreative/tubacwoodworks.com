17f3fc56af5014b13c680ea3aad499982b5cd1f2
Chantel Romero <momo@Chantels-MacBook-Pro.local>
2020-06-09 07:33:15 -0700

    created new seed file.

--- utils/scripts/seed_homepage.js
+++ utils/scripts/seed_homepage.js
@@ -1,46 +1,81 @@
-let seed_homepage = [
-    {
-        name: "homepage",
-        quoteTop: { h2: "Matthew Carpenter", p: "I am just stunned at how amazing my kitchen looks!", url: "tubacwoodworks.herokuapp.com/images/quote_2.jpg"},
-        quoteBottom: {  h2: "Elena Borne", p: "My home feels new again. Thank you Tubac Woodworks.", url: "tubacwoodworks.herokuapp.com/images/quote_1.jpg"},
-        checkerBox: { slotOne: "trusted", slotTwo: "quality", slotThree: "innovation" },
-        partners: {
-            partner_1: {
-                name: "Decore-Ative Specialties",
-                description: "Fine Crafted Cabinet Doors",
-                url: "https://www.decore.com",
-                picture: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/partner_1.jpg"
-            },
-            partner_2: {
-                name: "Decore-Ative Specialties",
-                description: "Countertops and Wooden Doors",
-                url: "https://www.decore.com",
-                picture: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/partner_2.jpg"
-            },
-            partner_3: {
-                name: "Decore-Ative Specialties",
-                description: "Custom wood doors",
-                url: "https://www.decore.com",
-                picture: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/partner_3.jpg"
-            },
-            partner_text: {
-                text: "take a look at our partners",
-                backgroundImage: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/partner_0.jpg"
-            }
+const seed_homepage = [{
+    "name": "homepage",
+    "quoteTop": {
+        "h2": "Barb Anderson",
+        "p": "I love my new Kitchen. It's changed the whole look and feel of the house!",
+        "url": "tubacwoodworks.herokuapp.com/images/quote_2.jpg"
+    },
+    "quoteBottom": {
+        "h2": "Elena Borne",
+        "p": "My home feels new again. Thank you Tubac Woodworks.",
+        "url": "tubacwoodworks.herokuapp.com/images/quote_1.jpg"
+    },
+    "checkerBox": {
+        "slotOne": "trusted",
+        "slotTwo": "quality",
+        "slotThree": "innovation"
+    },
+    "partners": {
+        "partner_1": {
+            "name": "Decore-Ative Specialties",
+            "description": "Fine Crafted Cabinet Doors",
+            "url": "https://www.decore.com",
+            "picture": "/cms/images/partner_2.jpg"
         },
-        hero: { logo: "Tubac Woodworks LLC", h2: `Tubac Woodworks Inc, building custom
-        cabinetry, etertainment center, bookcases,
-        desks, and a variety of other wood products
-        for home, office, or business.`},
-        portfolio: {
-            box_1: { title: "Kitchen & Bath", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.", background: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_1.jpg"},
-            box_2: { title: "Furniture", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.", background: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_2.jpg"},
-            box_3: { title: "Commercial", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.", background: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_3.jpg"},
-            box_4: { title: "Pro Tips", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.", background: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_4.jpg"},
-            box_5: { title: "See Our Work", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.", background: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_5.jpg"},
-            box_6: { title: "Get A Quote!", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.", background: "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_6.jpg"}
+        "partner_2": {
+            "name": "Decore-Ative Specialties",
+            "description": "Countertops and Wooden Doors",
+            "url": "https://www.decore.com",
+            "picture": "/cms/images/partner_3.jpg"
+        },
+        "partner_3": {
+            "name": "Decore-Ative Specialties",
+            "description": "Custom wood doors",
+            "url": "https://www.decore.com",
+            "picture": "/cms/images/partner_4.jpg"
+        },
+        "partner_text": {
+            "text": "take a look at our partners",
+            "backgroundImage": "/cms/images/partner_0.jpg"
+        }
+    },
+    "hero": {
+        "logo": "Tubac Woodworks LLC",
+        "h2": "Tubac Woodworks Inc, building custom cabinetry, entertainment center, bookcases, desks, and a variety of other wood products for home, office or business."
+    },
+    "portfolio": {
+        "box_1": {
+            "title": "Kitchen & Bath",
+            "description": "Get the great look and feel of an open kitchen.",
+            "background": "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_1.jpg"
+        },
+        "box_2": {
+            "title": "Pro Tips",
+            "description": "Learn more about what it takes to keep your wood furniture looking sharp",
+            "background": "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_2.jpg"
+        },
+        "box_3": {
+            "title": "commercial",
+            "description": "Fantastic woodworking tips and tricks. Find out more in our Pro Tips section.",
+            "background": "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_3.jpg"
+        },
+        "box_4": {
+            "title": "Pro Tips",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.",
+            "background": "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_4.jpg"
+        },
+        "box_5": {
+            "title": "See Our Work",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.",
+            "background": "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_5.jpg"
+        },
+        "box_6": {
+            "title": "Get A Quote!",
+            "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas amet at dolor.",
+            "background": "https://bobwehadababyitsaboy.s3-eu-west-1.amazonaws.com/portfolio_6.jpg"
         }
     }
-]
+}]
+    
 
 module.exports = seed_homepage;
